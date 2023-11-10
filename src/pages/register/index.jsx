251fe88e2a426ef49fc0ae629528c3f44885a289
Anderson <andersonmendes05@gmail.com>
2023-11-10 11:39:42 -0300

    [#165] Componente de Botão
    criação do componente de botão

--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -1,20 +1,25 @@
 import React from 'react'
 
 import Input from '../../components/forms/Input/index'
+import SubmitButton from '../../components/forms/submitButton/index'
 
-import { ReactComponent as Mail } from '../../assets/icons/mail.svg'
+import { ReactComponent as Padlock } from '../../assets/icons/padlock.svg'
 
 function Register() {
   return (
     <>
       <h1>Registro "teste"</h1>
+      <br />
       <Input
         type="password"
         name="password"
         id="password"
-        placeholder="Sua senha">
-        <Mail />
+        placeholder="Sua senha"
+      >
+        <Padlock />
       </Input>
+      <br />
+      <SubmitButton text="Registrar" />
     </>
   )
 }
