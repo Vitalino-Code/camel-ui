2d402ead5f0c31b2bf8189e367d93fba9ed25e3a
Anderson <andersonmendes05@gmail.com>
2023-11-13 14:38:42 -0300

    [#166] Criar a página Registro
    Criação da tela de registro.

--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -1,11 +1,68 @@
+import { Link } from 'react-router-dom'
+
 import AuthLayout from '../../components/layout/authLayout'
+import Input from '../../components/forms/Input'
+import Button from '../../components/common/Button'
+
+import logo from '../../assets/images/logo.png'
+import { FaArrowLeftLong } from 'react-icons/fa6'
+import { BiUser, BiLock } from 'react-icons/bi'
+import { LuMail } from 'react-icons/lu'
+
+import { Fildset, InfoArea } from './styles'
 
 function Register() {
   return (
     <>
-      <AuthLayout>
-        <h1>Componente 1</h1>
-        <h1>Componente 2</h1>
+      <AuthLayout reverse="true">
+        <Fildset>
+          <h2>Crie sua conta</h2>
+          <form>
+            <div>
+              <Input
+                type="text"
+                name="name"
+                id="name"
+                placeholder="Seu nome"
+                autoComplete="off"
+                icon={BiUser}
+              />
+              <Input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Seu e-mail"
+                autoComplete="off"
+                icon={LuMail}
+              />
+              <Input
+                type="password"
+                name="password"
+                id="password"
+                placeholder="Sua senha"
+                icon={BiLock}
+              />
+              <Input
+                type="password"
+                name="confirmPassword"
+                id="confirmPassword"
+                placeholder="Confirme sua senha"
+                icon={BiLock}
+              />
+            </div>
+            <Button text="Cadastrar" />
+          </form>
+        </Fildset>
+        <InfoArea>
+          <Link to={'/'}>
+            <img src={logo} alt="logo" />
+          </Link>
+          <h1>Melhor distribuidora da região</h1>
+          <h3>Material elétrico com o melhor preço, é na Camel</h3>
+          <Link to={'/login'}>
+            <FaArrowLeftLong /> Voltar para o login
+          </Link>
+        </InfoArea>
       </AuthLayout>
     </>
   )
---
+++ src/pages/register/styles.js
@@ -0,0 +1,68 @@
+import styled from 'styled-components'
+
+export const InfoArea = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 3.2rem;
+
+  text-align: center;
+
+  a > img {
+    width: 10rem;
+  }
+
+  h1 {
+    font-size: 3rem;
+  }
+
+  h3 {
+    font-size: 1.6rem;
+    color: #a8a8b3;
+    font-weight: bold;
+  }
+
+  a {
+    color: ${({ theme }) => theme.COLORS.ORANGE};
+    text-decoration: none;
+    font-weight: 600;
+    font-size: 1.4rem;
+    opacity: 80%;
+
+    display: flex;
+    align-items: center;
+    gap: 1.6rem;
+
+    transition: opacity 0.3s ease 0s;
+  }
+
+  @media (min-width: 425px) {
+    width: 38.5rem;
+  }
+
+  @media (min-width: 1024px) {
+    a > img {
+      width: 20rem;
+    }
+  }
+`
+
+export const Fildset = styled.div`
+  & > h2 {
+    font-size: 2rem;
+  }
+
+  & > form > div {
+    border: none;
+    margin: 3rem 0 6rem 0;
+
+    display: flex;
+    flex-direction: column;
+    gap: 1.5rem;
+  }
+
+  @media (min-width: 425px) {
+    width: 38.5rem;
+  }
+`
