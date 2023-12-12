e6e4e3413f60f5bd2dfc8cb2e787649d7b2819b8
Elias Neto <eliasneto321@outlook.com>
2023-12-12 20:50:58 -0300

    ♻️ adjust home route

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -15,7 +15,7 @@ export const RoutesManager = () => {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/signin" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
         </Routes>
