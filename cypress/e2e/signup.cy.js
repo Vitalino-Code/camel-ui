6052acf5f9d67ebdbd89ce521f868fadeef92da2
Elias Neto <eliasneto321@outlook.com>
2023-12-12 20:06:15 -0300

    ♻️ adjust routes

--- cypress/e2e/signup.cy.js
+++ cypress/e2e/signup.cy.js
@@ -1,14 +1,14 @@
 describe('Página de Perfil', () => {
-    it('Inserir CEP, rua, número, complemento, bairro, cidade, uf, telefone e clicar em salvar', () => {
-      cy.visit('http://127.0.0.1:5173/signup')
-      cy.get('#name').type('Ricardo Junior');
-      cy.wait(1500);
-      cy.get('#email').type('ricardo@vitalino.com');
-      cy.wait(1500);
-      cy.get('#password').type('123');
-      cy.wait(1500);
-      cy.get('#confirmedPassword').type('123');
-      cy.wait(1500);
-      cy.contains('Cadastrar').click();
-    })
+  it('Inserir CEP, rua, número, complemento, bairro, cidade, uf, telefone e clicar em salvar', () => {
+    cy.visit('http://127.0.0.1:5173/signup')
+    cy.get('#name').type('Ricardo Junior')
+    // cy.wait(1500)
+    cy.get('#email').type('ricardo@vitalino.com')
+    // cy.wait(1500)
+    cy.get('#password').type('123')
+    // cy.wait(1500)
+    cy.get('#confirmedPassword').type('123')
+    // cy.wait(1500)
+    cy.contains('Cadastrar').click()
   })
+})
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,25 @@
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-import { AppRoutes } from './app.routes'
-import { AuthRoutes } from './auth.routes'
+// import { AppRoutes } from './app.routes'
+// import { AuthRoutes } from './auth.routes'
+import Home from '../pages/home'
+import { SignIn } from '../pages/SignIn'
+import { SignUp } from '../pages/SignUp'
 
-import { useAuth } from '../contexts/AuthContext'
+// import { useAuth } from '../contexts/AuthContext'
 
 export const RoutesManager = () => {
-  const { user } = useAuth()
+  // const { user } = useAuth()
 
-  return <BrowserRouter>{user ? <AppRoutes /> : <AuthRoutes />}</BrowserRouter>
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
 }
