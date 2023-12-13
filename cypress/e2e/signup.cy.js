7f6b548fb3f8f692617b442b846d530b688bcf3a
Anderson <andersonmendes05@gmail.com>
2023-12-12 21:19:33 -0300

    [#VC - 185] Testes Cypress
    renomenado teste
    
    renomenado teste
    
    criação de novos testes de signup

--- cypress/e2e/signup.cy.js
+++ cypress/e2e/signup.cy.js
@@ -1,13 +1,91 @@
 describe('Página de Perfil', () => {
-  it('Inserir CEP, rua, número, complemento, bairro, cidade, uf, telefone e clicar em salvar', () => {
-    cy.visit('http://127.0.0.1:5173/signup')
-    cy.get('#name').type('Ricardo Junior')
+  it('Create user successfully', () => {
+    cy.visit('http://localhost:5173/signup')
+    cy.get('#name').type('Usuário Teste')
     // cy.wait(1500)
-    cy.get('#email').type('ricardo@vitalino.com')
+    cy.get('#email').type('user1@vitalino.com')
     // cy.wait(1500)
-    cy.get('#password').type('123')
+    cy.get('#password').type('123abc')
     // cy.wait(1500)
-    cy.get('#confirmedPassword').type('123')
+    cy.get('#confirmedPassword').type('123abc')
+    // cy.wait(1500)
+    cy.contains('Cadastrar').click()
+
+    cy.get('.Toastify__toast-body').should(
+      'contain',
+      'Sua conta foi criada com sucesso!',
+    )
+  })
+  it('Should return an error, as the password has no characters', () => {
+    cy.visit('http://localhost:5173/signup')
+    cy.get('#name').type('Usuário Teste')
+    // cy.wait(1500)
+    cy.get('#email').type('user@vitalino.com')
+    // cy.wait(1500)
+    cy.get('#password').type('123456')
+    // cy.wait(1500)
+    cy.get('#confirmedPassword').type('123456')
+    // cy.wait(1500)
+    cy.contains('Cadastrar').click()
+
+    cy.get('.Toastify__toast-body').should(
+      'contain',
+      'A senha deve ter mais de 6 caracteres e conter letras e números.',
+    )
+  })
+  it('Should return an error, as the password has no numbers', () => {
+    cy.visit('http://localhost:5173/signup')
+    cy.get('#name').type('Usuário Teste')
+    // cy.wait(1500)
+    cy.get('#email').type('user@vitalino.com')
+    // cy.wait(1500)
+    cy.get('#password').type('abcdef')
+    // cy.wait(1500)
+    cy.get('#confirmedPassword').type('abcdef')
+    // cy.wait(1500)
+    cy.contains('Cadastrar').click()
+
+    cy.get('.Toastify__toast-body').should(
+      'contain',
+      'A senha deve ter mais de 6 caracteres e conter letras e números.',
+    )
+  })
+  it('Should return an error, password and password confirmation are different', () => {
+    cy.visit('http://localhost:5173/signup')
+    cy.get('#name').type('Usuário Teste')
+    // cy.wait(1500)
+    cy.get('#email').type('user@vitalino.com')
+    // cy.wait(1500)
+    cy.get('#password').type('abcde1')
+    // cy.wait(1500)
+    cy.get('#confirmedPassword').type('abcdef1')
+    // cy.wait(1500)
+    cy.contains('Cadastrar').click()
+
+    cy.get('.Toastify__toast-body').should(
+      'contain',
+      'As senhas devem ser iguais.',
+    )
+  })
+  it('Should return an error, E-mail already registered', () => {
+    cy.visit('http://localhost:5173/signup')
+    cy.get('#name').type('Usuário Teste')
+    // cy.wait(1500)
+    cy.get('#email').type('user@vitalino.com')
+    // cy.wait(1500)
+    cy.get('#password').type('asd123')
+    // cy.wait(1500)
+    cy.get('#confirmedPassword').type('asd123')
+    // cy.wait(1500)
+    cy.contains('Cadastrar').click()
+    cy.visit('http://localhost:5173/signup')
+    cy.get('#name').type('Novo Usuário')
+    // cy.wait(1500)
+    cy.get('#email').type('user@vitalino.com')
+    // cy.wait(1500)
+    cy.get('#password').type('asd123')
+    // cy.wait(1500)
+    cy.get('#confirmedPassword').type('asd123')
     // cy.wait(1500)
     cy.contains('Cadastrar').click()
   })
