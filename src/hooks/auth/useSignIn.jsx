a2fa25396b936ba328bdd83b0a3cdbac544e64b2
Anderson <andersonmendes05@gmail.com>
2023-12-15 20:33:04 -0300

    fix: case-sensitivity

--- src/hooks/auth/useSignIn.jsx
+++ src/hooks/auth/useSignIn.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 
-import { useAuth } from '../../contexts/AuthContext'
+import { useAuth } from '../../contexts/authContext'
 
 const useSignIn = () => {
   const [error, setError] = useState(null)
