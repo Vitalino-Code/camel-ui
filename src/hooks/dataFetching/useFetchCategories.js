8b9cb4057e4ceac1a513fc60c76eb48b733ca17b
ArthurCosta15 <arthurcesar252@gmail.com>
2024-02-29 20:16:09 -0300

    [#194] Categorias do Header
     Integração das Categorias no Header
    
    corrigindo erros do prettier
    
    criação do hook
    
    corrigindo erros do lint
    
    [#194] Categorias do Header
    
     Integração das Categorias no Header
    
    corrigindo erros do prettier
    
    criação do hook
    
    [#194] Categorias do Header
    
     Integração das Categorias no Header
    
    corrigindo erros do prettier

---
+++ src/hooks/dataFetching/useFetchCategories.js
@@ -0,0 +1,52 @@
+import { useState } from 'react'
+import { api } from '../../services/api.js'
+
+const useFetchCategories = () => {
+  const [error, setError] = useState(null)
+  const [categories, setCategories] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+
+
+  const fetchCategories = async (successCallback) => {
+    try {
+      setIsLoading(true)
+      const response = await api.get(`/categories`)
+      setCategories(response.data)
+      successCallback(response.data)
+    } catch (error) {
+      setError(error)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  return { categories, isLoading, error, fetchCategories }
+}
+
+export { useFetchCategories }
+
+
+import { useState } from 'react'
+import { api } from '../../services/api.js'
+
+const useFetchCategories = () => {
+  const [error, setError] = useState(null)
+  const [categories, setCategories] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  const fetchCategories = async successCallback => {
+    try {
+      setIsLoading(true)
+      const response = await api.get(`/categories`)
+      setCategories(response.data)
+      successCallback(response.data)
+    } catch (error) {
+      setError(error)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  return { categories, isLoading, error, fetchCategories }
+}
+
+export { useFetchCategories }
