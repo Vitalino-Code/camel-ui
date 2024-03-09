31e55a934b36737c25c393970b5e87e3b1107367
Anderson <andersonmendes05@gmail.com>
2024-03-09 15:45:49 -0300

    fix lint

--- src/pages/shoppingCart/index.jsx
+++ src/pages/shoppingCart/index.jsx
@@ -15,9 +15,9 @@ import {
   Total,
 } from './styles'
 
-import img1 from '../../assets/images/img1.jpg'
-import img2 from '../../assets/images/img2.jpg'
-import img3 from '../../assets/images/img3.jpg'
+// import img1 from '../../assets/images/img1.jpg'
+// import img2 from '../../assets/images/img2.jpg'
+// import img3 from '../../assets/images/img3.jpg'
 
 // const itens = [
 //   {
