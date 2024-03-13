2b369c0166943b92d5c7fb67390d50e20f880535
ArthurCosta15 <arthurcesar252@gmail.com>
2024-03-13 09:18:04 -0300

    [#VC-227] Slider de Marcas
    criação do slider de marcas
    
    corrigindo error cypress

---
+++ src/components/common/brands/styles.js
@@ -0,0 +1,56 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  background-color: ${({ theme }) => theme.COLORS.WHITE};
+  color: ${({ theme }) => theme.COLORS.BLACK};
+  width: 75%;
+  padding: 2.4rem;
+  border-radius: 1rem;
+  text-align: center;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 7rem;
+
+  .swiper {
+    width: 100%;
+    height: 100%;
+  }
+
+  .swiper-slide {
+    text-align: center;
+    font-size: 18px;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .swiper-slide img {
+    display: block;
+    width: 100%;
+    max-width: 100%;
+    object-fit: cover;
+  }
+
+  h2 {
+    font-size: 1.8rem;
+    margin-bottom: 2.4rem;
+  }
+  .swiper-button-prev,
+  .swiper-button-next {
+    color: #f08605;
+  }
+`
+
+export const SwiperContainer = styled.ul`
+  img {
+    width: 10rem;
+    height: 10rem;
+    border-radius: 50%;
+  }
+
+  span {
+    color: ${({ theme }) => theme.COLORS.BLACK};
+    font-size: 1.6rem;
+  }
+`
