d5d30b83c2faaa42de63ad36c2027d2bed592aa1
Anderson <99482145+andersonalbuqq@users.noreply.github.com>
2024-04-15 17:14:29 -0300

    Update useFetchProducts.js

--- src/hooks/dataFetching/useFetchProducts.js
+++ src/hooks/dataFetching/useFetchProducts.js
@@ -27,7 +27,6 @@ const useFetchProducts = () => {
       const { data } = response
       const pageCount = Math.ceil(response.headers['x-count'] / perPage)
       setProducts(data)
-      console.log(data)
       successCallback({ products: data, pageCount })
     } catch (error) {
       setError(error)
